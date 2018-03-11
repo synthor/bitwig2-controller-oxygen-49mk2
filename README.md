5ef644ca3190dbf7e05edf37f19540eabb897c82
synthor <thorsten.munsch@synthor.de>
2018-03-11 18:35:32 +0100

    fixed missing noteinput to get the keys working

---
+++ README.md
@@ -0,0 +1,20 @@
+# Bitwig Controller Extension for M-Audio Oxygen 49 MKII
+
+Just adapted the known Javascript v1-API control script to the new Java API which is used since Bitwig Studio 2.
+
+Successfully tested on Bitwig Studio 2.2.2 on Ubuntu Linux 17.10 with an Akai MPD 218 as second controller.
+
+Haven't digged further into logic errors, bugs or enhancements because I just wanted to get my Oxygen running again with Bitwig.
+
+Fork or let me know if you find one. Enjoy!
+
+## Installation
+
+Download the compiled extension from [here](https://github.com/FIXURL) and put it into your `~/Bitwig Studio/Extensions` folder.
+Note that the `Extensions` path is probably different for Windows or Mac.
+
+## Develoment
+
+Just install an IDE of your favour (e.g. Netbeans) and create a project with existing sources.
+
+You'll need Java JDK 1.8 and Maven installed to build even without an IDE.
--- src/main/java/com/maudio/Oxygen49Extension.java
+++ src/main/java/com/maudio/Oxygen49Extension.java
@@ -7,6 +7,7 @@ import com.bitwig.extension.controller.api.Transport;
 import com.bitwig.extension.controller.ControllerExtension;
 import com.bitwig.extension.controller.api.CursorDevice;
 import com.bitwig.extension.controller.api.CursorTrack;
+import com.bitwig.extension.controller.api.NoteInput;
 import com.bitwig.extension.controller.api.Parameter;
 import com.bitwig.extension.controller.api.RemoteControlsPage;
 import com.bitwig.extension.controller.api.UserControlBank;
@@ -18,6 +19,7 @@ public class Oxygen49Extension extends ControllerExtension
     private CursorTrack cursortrack;
     private CursorDevice cursordevice;
     private RemoteControlsPage remotecontrols;
+    private NoteInput noteinput;
     
     protected Oxygen49Extension(final Oxygen49ExtensionDefinition definition, final ControllerHost host)
     {
@@ -33,6 +35,10 @@ public class Oxygen49Extension extends ControllerExtension
         host.getMidiInPort(0).setMidiCallback((ShortMidiMessageReceivedCallback) this::onMidi0);
         host.getMidiInPort(0).setSysexCallback(this::onSysex0);
 
+        // Add a note input
+        this.noteinput = host.getMidiInPort(0).createNoteInput("", "??????");
+        this.noteinput.setShouldConsumeEvents(false);
+        
         // Map CC 20 - 27 to device parameters
         this.transport = host.createTransport();
         this.cursortrack = host.createCursorTrack(3, 0);
@@ -43,7 +49,7 @@ public class Oxygen49Extension extends ControllerExtension
             Parameter p = this.remotecontrols.getParameter(i);
             p.setIndication(true);
             p.setLabel("P" + (i + 1));
-//            p.markInterested();
+            p.markInterested();
         }
 
         // Make the rest freely mappable
@@ -53,7 +59,7 @@ public class Oxygen49Extension extends ControllerExtension
             if( !isIndexInDeviceCCRange(i) ) {
                 int index = userIndexFromCC(i);
                 this.usercontrols.getControl(index).setLabel("CC" + i);
-//                this.usercontrols.getControl(index).markInterested();
+                this.usercontrols.getControl(index).markInterested();
             }
         }
 
@@ -75,7 +81,9 @@ public class Oxygen49Extension extends ControllerExtension
     // Returns true if the given index is in the device's cc range or false if not
     private Boolean isIndexInDeviceCCRange( int index )
     {
-        return index >= CC.DEVICE_START_CC && index <= CC.DEVICE_END_CC;
+        Boolean b = index >= CC.DEVICE_START_CC && index <= CC.DEVICE_END_CC;
+        getHost().showPopupNotification("isIndexInDeviceCCRange(" + index + ") = " + b.toString());
+        return b;
     }
     
     // Returns the user index from the given cc value
@@ -91,103 +99,82 @@ public class Oxygen49Extension extends ControllerExtension
     // MIDI-Callback
     private void onMidi0( ShortMidiMessage msg )
     {
+        if( !msg.isControlChange() ) {
+            return;
+        }
+        
         int data1 = msg.getData1();
         int data2 = msg.getData2();
-        int status = msg.getStatusByte();
-        
-        if( status == ShortMidiMessage.CONTROL_CHANGE )
+  
+        if( isIndexInDeviceCCRange(data1) )
         {
-            if( isIndexInDeviceCCRange(data1) )
+            int index = -1;
+
+            if( data1 == 75) { index = 0; }
+            if( data1 == 76) { index = 1; }
+            if( data1 == 92) { index = 2; }
+            if( data1 == 95) { index = 3; }
+            if( data1 == 10) { index = 4; }
+            if( data1 == 77) { index = 5; }
+            if( data1 == 78) { index = 6; }
+            if( data1 == 79) { index = 7; }
+
+            if( index > -1 ) 
             {
-                int index = -1;
-                
-                if( data1 == 75) { index = 0; }
-                if( data1 == 76) { index = 1; }
-                if( data1 == 92) { index = 2; }
-                if( data1 == 95) { index = 3; }
-                if( data1 == 10) { index = 4; }
-                if( data1 == 77) { index = 5; }
-                if( data1 == 78) { index = 6; }
-                if( data1 == 79) { index = 7; }
-                
-                if( index > -1 ) 
+                this.remotecontrols.getParameter(index).value().set(data2, 128);
+            }
+        }
+        // Handle transport-buttons and trackselection
+        else if( (data1 >= CC.PREV_TRACK && data1 <= CC.RECORD && data1 != 112) && 
+                  data2 > 0 )
+        {
+            switch( data1 ) {
+                case CC.PREV_TRACK:
+                    this.cursortrack.selectPrevious();
+                    break;
+                case CC.NEXT_TRACK:
+                    this.cursortrack.selectNext();
+                    break;
+                case CC.LOOP:
+                    //this.transport.toggleLoop(); // Deprecated
+                    this.transport.isArrangerLoopEnabled().toggle();
+                    break;
+                case CC.REWIND:
+                    this.transport.rewind();
+                    break;
+                case CC.FORWARD:
+                    this.transport.fastForward();
+                    break;
+                case CC.STOP:
+                    this.transport.stop();
+                    break;
+                case CC.PLAY:
+                    this.transport.play();
+                    break;
+                case CC.RECORD:
+                    this.transport.record();
+                    break;
+            }
+        }
+        else if( data1 >= CC.LOWEST_CC && data1 <= CC.HIGHEST_CC )
+        {
+            // Handle slider as trackvolume
+            if( data1 == CC.SLIDER )
+            {
+                this.cursortrack.getVolume().set(data2, 128);
+            }
+            else
+            {
+                // Handle CC 02-119
+                if( data1 >= CC.LOWEST_CC && data1 <= CC.HIGHEST_CC )
                 {
-                    this.remotecontrols.getParameter(index).value().set(data2, 128);
-                }
-                // Handle transport-buttons and trackselection
-                else if( (data1 >= CC.PREV_TRACK && data1 <= CC.RECORD && data1 != 112) && 
-                          data2 > 0 )
-                {
-                    switch( data1 ) {
-                        case CC.PREV_TRACK:
-                            this.cursortrack.selectPrevious();
-                            break;
-                        case CC.NEXT_TRACK:
-                            this.cursortrack.selectNext();
-                            break;
-                        case CC.LOOP:
-                            this.transport.toggleLoop();
-                            break;
-                        case CC.REWIND:
-                            this.transport.rewind();
-                            break;
-                        case CC.FORWARD:
-                            this.transport.fastForward();
-                            break;
-                        case CC.STOP:
-                            this.transport.stop();
-                            break;
-                        case CC.PLAY:
-                            this.transport.play();
-                            break;
-                        case CC.RECORD:
-                            this.transport.record();
-                            break;
-                    }
-                }
-                else if( data1 >= CC.LOWEST_CC && data1 <= CC.HIGHEST_CC )
-                {
-                    // Handle slider as trackvolume
-                    if( data1 == CC.SLIDER )
-                    {
-                        this.cursortrack.getVolume().set(data2, 128);
-                    }
-                    else
-                    {
-                        // Handle CC 02-119
-                        if( data1 >= CC.LOWEST_CC && data1 <= CC.HIGHEST_CC )
-                        {
-                            int userindex = userIndexFromCC(data1);
-                            this.usercontrols.getControl(userindex).value().set(data2, 128);
-                        }
-                    }
+                    int userindex = userIndexFromCC(data1);
+                    this.usercontrols.getControl(userindex).value().set(data2, 128);
                 }
             }
         }
     }
     
     // SysEx-Callback
-    private void onSysex0( final String data )
-    {
-        getHost().println("Somehow triggered a sysex: " + data);
-        
-        // MMC Transport Controls:
-        switch( data ) {
-            case "f07f7f0605f7":
-                this.transport.rewind();
-                break;
-            case "f07f7f0604f7":
-                this.transport.fastForward();
-                break;
-            case "f07f7f0601f7":
-                this.transport.stop();
-                break;
-            case "f07f7f0602f7":
-                this.transport.play();
-                break;
-            case "f07f7f0606f7":
-                this.transport.record();
-                break;
-        }
-    }
+    private void onSysex0( final String data ) {}
 }
